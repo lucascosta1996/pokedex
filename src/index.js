9489618d0529817d681484c640e4d279ef26129d
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-04-03 11:29:01 -0300

    Finish global style and main route

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './index.css'
 import PokemonsContextProvider from './context/PokemonsContext'
-import Pokemons from './components/Pokemons'
+import PokemonInfo from './components/PokemonInfo'
+import Header from './layout/Header'
+import ListWrapper from './layout/ListWrapper'
 
 function App() {
   return(
       <Router>
         <PokemonsContextProvider>
-          <Pokemons />
+          <Header />
+          <Route exact path="/pokedex">
+            <ListWrapper />
+          </Route>
+          <Route path="/pokedex/:id" component={ PokemonInfo } />
         </PokemonsContextProvider>
       </Router>
 
